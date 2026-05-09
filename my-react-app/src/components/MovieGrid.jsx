40f52bd9356b38153da380dcb2d4b58c52be640f
esccot <esccot.dev@gmail.com>
2026-05-09 14:02:49 +0530

    update grid placement

--- my-react-app/src/components/MovieGrid.jsx
+++ my-react-app/src/components/MovieGrid.jsx
@@ -1,13 +1,28 @@
 import "./MovieGrid.css";
 
-function MovieGrid({ movies, alt }) {
+function MovieGrid({ movies }) {
+  console.log("MovieGrid rendering with movies:", movies);
+  if(!movies){
+    if (movies.length === 0) {
+    return <p>No movies found. Try a different search.</p>;
+  }
+  }
+  
   return (
     <div className="movie-grid">
       {movies.map((movie) => (
-        <div className="movie-card" key={movie.imdbID}>
-          <img className="card-img" src={movie.Poster} alt={alt} />
-          <p>{movie.Title}</p>
-          <p>{movie.Year}</p>
+        <div className="movie-card" key={movie.id}>
+          {movie.poster_path ? (
+            <img
+              className="card-img"
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
+          ) : (
+            <div className="no-poster">No Image</div>
+          )}
+          <p>{movie.title}</p>
+          <p>{movie.release_date}</p>
         </div>
       ))}
     </div>
