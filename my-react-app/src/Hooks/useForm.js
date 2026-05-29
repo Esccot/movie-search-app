3b544ba7f9cde80b5b202a05aca925f0d5f11f3c
esccot <esccot.dev@gmail.com>
2026-05-29 22:15:44 +0530

    add funtionality for form

---
+++ my-react-app/src/Hooks/useForm.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function useForm() {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  function handleChange(e) {
+    setShowPassword(false);
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      console.log("submitted");
+      const response = await fetch("http://localhost:3000/app/signup", {
+        method: "post",
+        headers: {
+          "content-Type": "application/json", // means the data is json
+        },
+        body: JSON.stringify(form),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (response.ok) {
+        navigate("/app/user/profile");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return {
+    form,
+    handleChange,
+    handleSubmit,
+    setShowPassword,
+    showPassword,
+  };
+}
+
+export default useForm;
