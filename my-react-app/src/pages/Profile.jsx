6ec82fb7a70477bf799982791313b5126c237fd5
esccot <esccot.dev@gmail.com>
2026-05-29 22:17:25 +0530

    create profile section for users

---
+++ my-react-app/src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import "../components/Profile.css";
+
+function Profile() {
+  return (
+    <div>
+      <h1 className="profile-heading">hello USER</h1>
+    </div>
+  );
+}
+
+export default Profile;
