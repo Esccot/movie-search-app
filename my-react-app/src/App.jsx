3c5b5c558cf00a5c70ec11464a4d2c5f61c4f3c5
esccot <esccot.dev@gmail.com>
2026-06-10 15:35:51 +0530

    add new routes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetail";
-import SignIn from "./components/Form";
+import SignIn from "./components/SignIn";
 import Profile from "./pages/Profile";
+import MovieCard from "./components/MovieCard";
+import Navbar from "./components/Navbar";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/movie/:id" element={<MovieDetails />} />
-      <Route path="/app/login" element={<SignIn />} />
-      <Route path="/app/user/profile" element={<Profile />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/app/user/signup" element={<SignUp />} />
+        <Route path="/app/user/profile" element={<Profile />} />
+        <Route path="app/user/signin" element={<SignIn />} />
+      </Routes>
+    </>
   );
 }
 
