b00ba1ad9cccce78c1302eac88c7fe2f32af968a
esccot <esccot.dev@gmail.com>
2026-06-16 12:14:24 +0530

    add dynamic user name

--- my-react-app/src/pages/Profile.jsx
+++ my-react-app/src/pages/Profile.jsx
@@ -1,9 +1,11 @@
 import "../components/Profile.css";
+import useProfile from "../Hooks/useProfile";
 
 function Profile() {
+  const { user } = useProfile();
   return (
     <div>
-      <h1 className="profile-heading">hello USER</h1>
+      <h1 className="profile-heading">hello {user?.username}</h1>
     </div>
   );
 }
